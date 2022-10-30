0cc7809001c9819ecf97287a54a7192315dc32ff
abstractjazz <evan@heyevan.com>
2022-10-30 16:12:05 -0700

    refactor

--- pages/blog.js
+++ pages/blog.js
@@ -5,15 +5,14 @@ export default function Blog(allPosts) {
     return (
      
       <div className="">
-        {console.log(allPosts.posts)}
-       <AllPosts postInfo={allPosts.posts}/>
+       <AllPosts postInfo = {allPosts.allPosts}/>
       </div>
     )
   }
   
   export const getServerSideProps = async () => {
       const query = '*[_type == "post"]'
-      const posts = await client.fetch(query)
+      const allPosts = await client.fetch(query)
       return {
         props: {
           allPosts
