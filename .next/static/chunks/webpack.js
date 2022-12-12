276996a6400c2cf7ae07dbde037aef4163371024
Evan <evan@heyevan.com>
2022-12-11 20:37:16 -0800

    adds success page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "217ec73d363e9afe"; }
+/******/ 		__webpack_require__.h = function() { return "6d78157fa00ef69f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,5 +1,74 @@
 import Stripe from 'stripe';
 
+const stripeCountryObjs = [
+  { country: 'Argentina', code: 'AR' },
+  { country: 'Australia', code: 'AU' },
+  { country: 'Austria', code: 'AT' },
+  { country: 'Belgium', code: 'BE' },
+  { country: 'Bolivia', code: 'BO' },
+  { country: 'Brazil', code: 'BR' },
+  { country: 'Bulgaria', code: 'BG' },
+  { country: 'Canada', code: 'CA' },
+  { country: 'Chile', code: 'CL' },
+  { country: 'Colombia', code: 'CO' },
+  { country: 'Costa Rica', code: 'CR' },
+  { country: 'Croatia', code: 'HR' },
+  { country: 'Cyprus', code: 'CY' },
+  { country: 'Czech Republic', code: 'CZ' },
+  { country: 'Denmark', code: 'DK' },
+  { country: 'Dominican Republic', code: 'DO' },
+  { country: 'Estonia', code: 'EE' },
+  { country: 'Finland', code: 'FI' },
+  { country: 'France', code: 'FR' },
+  { country: 'Germany', code: 'DE' },
+  { country: 'Greece', code: 'GR' },
+  { country: 'Hong Kong SAR China', code: 'HK' },
+  { country: 'Hungary', code: 'HU' },
+  { country: 'Iceland', code: 'IS' },
+  { country: 'India', code: 'IN' },
+  { country: 'Indonesia', code: 'ID' },
+  { country: 'Ireland', code: 'IE' },
+  { country: 'Israel', code: 'IL' },
+  { country: 'Italy', code: 'IT' },
+  { country: 'Japan', code: 'JP' },
+  { country: 'Latvia', code: 'LV' },
+  { country: 'Liechtenstein', code: 'LI' },
+  { country: 'Lithuania', code: 'LT' },
+  { country: 'Luxembourg', code: 'LU' },
+  { country: 'Malta', code: 'MT' },
+  { country: 'Mexico ', code: 'MX' },
+  { country: 'Netherlands', code: 'NL' },
+  { country: 'New Zealand', code: 'NZ' },
+  { country: 'Norway', code: 'NO' },
+  { country: 'Paraguay', code: 'PY' },
+  { country: 'Peru', code: 'PE' },
+  { country: 'Poland', code: 'PL' },
+  { country: 'Portugal', code: 'PT' },
+  { country: 'Romania', code: 'RO' },
+  { country: 'Singapore', code: 'SG' },
+  { country: 'Slovakia', code: 'SK' },
+  { country: 'Slovenia', code: 'SI' },
+  { country: 'Spain', code: 'ES' },
+  { country: 'Sweden', code: 'SE' },
+  { country: 'Switzerland', code: 'CH' },
+  { country: 'Thailand', code: 'TH' },
+  { country: 'Trinidad & Tobago', code: 'TT' },
+  { country: 'United Arab Emirates', code: 'AE' },
+  { country: 'United Kingdom', code: 'GB' },
+  { country: 'United States', code: 'US' },
+  { country: 'Uruguay', code: 'UY' }
+]
+
+const stripeCountryCodeArr = stripeCountryObjs.map((countryObj)=>{
+ const codeArray=[]
+  const{code} = countryObj
+   codeArray.push(code)
+
+  return codeArray.join(' ')
+})
+
+
+
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
@@ -13,7 +82,7 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types:['card'],
         billing_address_collection: 'auto',
-        shipping_address_collection: {allowed_countries: ['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF',]},
+        shipping_address_collection: {allowed_countries: stripeCountryCodeArr},
         shipping_options: [
             {shipping_rate: 'shr_1MCyrXKz69mUHYoDaik2Q9zA'},
             {shipping_rate: 'shr_1MCyurKz69mUHYoDd5AYtLUg'}
@@ -38,8 +107,8 @@ export default async function handler(req, res) {
             },
             quantity: item.quantity
         }}),
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       }
 
      const session = await stripe.checkout.sessions.create(params);
@@ -55,4 +124,11 @@ export default async function handler(req, res) {
   }
  
 }
+
+
+
+
+
+
+
 
