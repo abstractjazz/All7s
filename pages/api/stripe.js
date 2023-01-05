f8fe7710af83a005e89bd5ef88c93495d312bfa3
Evan <evan@heyevan.com>
2023-01-05 15:38:43 -0500

    brings backk 200 status

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -113,8 +113,7 @@ export default async function handler(req, res) {
       
      const session = await stripe.checkout.sessions.create(params);
      console.log('SESSIONID', session.id)
-    //  res.status(200).json(session);
-      res.redirect(303, session.url);
+     res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
