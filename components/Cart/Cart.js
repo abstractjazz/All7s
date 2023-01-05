fac9b06539350c4ba6032870b54ee78c81a59607
Evan <evan@heyevan.com>
2023-01-05 14:37:15 -0500

    adds bearer auth

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -19,6 +19,7 @@ const handleCheckout = async () => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization' : `Bearer ${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`
             },
             body: JSON.stringify(cartItems)
         })
